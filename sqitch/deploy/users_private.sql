63c054effd13fec36caeda6d96bb33fd338953e2
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-08-29 14:46:27 -0500

    add id to users private table

--- sqitch/deploy/users_private.sql
+++ sqitch/deploy/users_private.sql
@@ -5,9 +5,10 @@
 BEGIN;
 
 CREATE TABLE ftlc_private.users(
-  user_id       UUID PRIMARY KEY REFERENCES ftlc.users(id) ON DELETE CASCADE,
-  email         CITEXT NOT NULL UNIQUE CHECK(email ~* '^.+@.+\..+$'),
-  password_hash TEXT NOT NULL
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID UNIQUE REFERENCES ftlc.users(id) ON DELETE CASCADE,
+    email CITEXT NOT NULL UNIQUE CHECK(email ~* '^.+@.+\..+$'),
+    password_hash TEXT NOT NULL
 );
 
 COMMIT;
