ed554eb1ace12f9839ef47c453022606534e0f34
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-08-30 15:23:32 -0500

    add attendance variable to event registration table

--- sqitch/deploy/event_registration.sql
+++ sqitch/deploy/event_registration.sql
@@ -8,7 +8,8 @@ CREATE TABLE ftlc.event_registration(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     student UUID REFERENCES ftlc.students(id),
     event_date UUID REFERENCES ftlc.event_dates(id),
-    registered_on TIMESTAMP DEFAULT NOW()
+    registered_on TIMESTAMP DEFAULT NOW(),
+    attendance BOOLEAN
 );
 
 COMMIT;
