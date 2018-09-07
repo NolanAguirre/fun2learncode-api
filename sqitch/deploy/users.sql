208a4249d3387ea43cca772ab449858338fe14f0
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-09-06 22:11:27 -0500

    remove contact email and contact phone

--- sqitch/deploy/users.sql
+++ sqitch/deploy/users.sql
@@ -8,8 +8,8 @@ CREATE TABLE ftlc.users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     first_name CITEXT,
     last_name CITEXT,
-    contact_email CITEXT UNIQUE CHECK(contact_email ~* '^.+@.+\..+$'),
-    contact_phone varchar(12),
+    -- contact_email CITEXT UNIQUE CHECK(contact_email ~* '^.+@.+\..+$'),
+    -- contact_phone varchar(12),
     role ftlc.role_type
 );
 
