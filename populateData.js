27840ae6b5e712460dc535c2dada975bbeb4bd72
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-09-06 21:30:25 -0500

    update data and script to populate more realistic data

--- populateData.js
+++ populateData.js
@@ -20,7 +20,8 @@ async function populateDatabse() {
     let event_dates = JSON.parse(fs.readFileSync('sqitch/data/event_dates.json', 'utf8'));
     let event_registration = JSON.parse(fs.readFileSync('sqitch/data/event_registration.json', 'utf8'));
     let event_logs = JSON.parse(fs.readFileSync('sqitch/data/event_logs.json', 'utf8'));
-    function randomItem(data){
+
+    function randomItem(data) {
         return data[Math.floor(Math.random() * data.length)];
     }
     await addData(users);
@@ -53,27 +54,30 @@ async function populateDatabse() {
     await addData(students);
     await addData(addresses);
     await addData(activity_catagories);
-    activities.data.map((element)=>{
-        element.type = randomItem(activity_catagories.data).id;
+    activities.data.map((element) => {
+        element.type = activity_catagories.data.filter((el) => {
+            return element.type == el.name;
+        })[0].id;
         return element;
     });
     await addData(activities);
     events.data.map((element) => {
         element.event_type = randomItem(activities.data).id;
         element.address = randomItem(addresses.data).id;
+        return element;
     })
     await addData(events);
-    for(let x = 0; x < 1000; x++){
+    for (let x = 0; x < 1000; x++) {
         let temp = {};
-        temp.start_date = new Date(+new Date() + Math.floor(Math.random()*5629740000));
-        temp.end_date = new Date(+temp.start_date +  Math.floor(Math.random()*629740000));
+        temp.start_date = new Date(+new Date() + Math.floor(Math.random() * 5629740000));
+        temp.end_date = new Date(+temp.start_date + Math.floor(Math.random() * 629740000));
         temp.event = randomItem(events.data).id;
         event_dates.data.push(temp);
     }
     await addData(event_dates)
-    event_dates.data.forEach((element)=>{
-        let random =Math.floor(Math.random() * 10) + 1;
-        for(let x = 0; x < random; x++){
+    event_dates.data.forEach((element) => {
+        let random = Math.floor(Math.random() * 10) + 1;
+        for (let x = 0; x < random; x++) {
             let temp = {};
             temp.student = randomItem(students.data).id;
             temp.event_date = element.id;
@@ -81,7 +85,7 @@ async function populateDatabse() {
         }
     })
     await addData(event_registration)
-    event_registration.data.forEach((element)=>{
+    event_registration.data.forEach((element) => {
         let temp = {};
         temp.student = element.student;
         temp.event_date = element.event_date;
