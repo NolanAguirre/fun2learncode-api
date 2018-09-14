0cec93a572e850f8c400c0e0eb3895ec2c8d2c24
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-09-13 22:42:36 -0500

    add select priviliges

--- sqitch/deploy/privileges.sql
+++ sqitch/deploy/privileges.sql
@@ -13,11 +13,15 @@ GRANT SELECT ON ftlc.address TO ftlc_user, ftlc_anonymous, ftlc_instructor, ftlc
 -- event dates
 GRANT SELECT ON ftlc.event_dates TO ftlc_user, ftlc_anonymous, ftlc_instructor, ftlc_owner, ftlc_student;
 -- event_logs
+GRANT SELECT ON ftlc.event_logs TO ftlc_user, ftlc_instructor, ftlc_owner, ftlc_student;
 -- event_prerequisites
 GRANT SELECT ON ftlc.event_prerequisites TO ftlc_user, ftlc_anonymous, ftlc_instructor, ftlc_owner, ftlc_student;
 -- event_registration
+GRANT SELECT ON ftlc.event_registration TO ftlc_user, ftlc_instructor, ftlc_owner, ftlc_student;
 -- events
 GRANT SELECT ON ftlc.events TO ftlc_user, ftlc_anonymous, ftlc_instructor, ftlc_owner, ftlc_student;
 -- students
+GRANT SELECT ON ftlc.students TO ftlc_user, ftlc_instructor, ftlc_owner, ftlc_student;
 -- users
+GRANT SELECT ON ftlc.users TO ftlc_user, ftlc_instructor, ftlc_owner, ftlc_student;
 COMMIT;
