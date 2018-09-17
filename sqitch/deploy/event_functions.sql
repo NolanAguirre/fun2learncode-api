59653a2fadd18c2348f309f0ac1b66503f4e2fa6
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-09-17 14:27:02 -0500

    create event months by student query

--- sqitch/deploy/event_functions.sql
+++ sqitch/deploy/event_functions.sql
@@ -6,7 +6,13 @@ BEGIN;
 
 CREATE FUNCTION ftlc.events_by_student(student_id UUID) RETURNS SETOF ftlc.events AS $$
     BEGIN
-        RETURN QUERY SELECT * FROM ftlc.events WHERE id IN (SELECT DISTINCT event FROM ftlc.event_dates WHERE id IN (SELECT event_date FROM ftlc.event_registration WHERE student = student_id));
+        RETURN QUERY SELECT * FROM ftlc.events WHERE id IN (SELECT DISTINCT event FROM ftlc.event_dates WHERE id IN (SELECT event_date FROM ftlc.event_registration WHERE student = student_id)) ORDER BY event_type ASC;
+    END;
+$$ LANGUAGE PLPGSQL STABLE;
+
+CREATE FUNCTION ftlc.event_months_by_student(student_id UUID) RETURNS SETOF ftlc.event_months AS $$
+    BEGIN
+        RETURN QUERY SELECT * FROM ftlc.event_months WHERE ftlc.event_dates.event IN (SELECT DISTINCT event FROM ftlc.event_dates WHERE id IN (SELECT event_date FROM ftlc.event_registration WHERE student = student_id)) ORDER BY event_type ASC;
     END;
 $$ LANGUAGE PLPGSQL STABLE;
 
--- sqitch/revert/event_functions.sql
+++ sqitch/revert/event_functions.sql
@@ -4,4 +4,6 @@ BEGIN;
 
 DROP FUNCTION ftlc.events_by_student(UUID);
 
+DROP FUNCTION ftlc.event_months_by_student(UUID);
+
 COMMIT;
