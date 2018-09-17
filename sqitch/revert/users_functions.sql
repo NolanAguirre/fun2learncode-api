5f8e3d00612c9475b5012efae998e447320b14cb
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-09-17 13:32:31 -0500

    update revert script

--- sqitch/revert/users_functions.sql
+++ sqitch/revert/users_functions.sql
@@ -2,6 +2,10 @@
 
 BEGIN;
 
+DROP FUNCTION ftlc.get_user_data();
+
+DROP FUNCTION ftlc.get_id();
+
 DROP FUNCTION ftlc.register_user(CITEXT, CITEXT, CITEXT, TEXT);
 
 DROP FUNCTION ftlc.register_student(CITEXT, CITEXT, CITEXT, TEXT);
