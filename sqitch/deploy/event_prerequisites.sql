e7aed2537c88663164956ba0be15f1929d182b77
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-09-21 15:02:41 -0500

    add id to event prerequisites

--- sqitch/deploy/event_prerequisites.sql
+++ sqitch/deploy/event_prerequisites.sql
@@ -4,9 +4,10 @@
 BEGIN;
 
 CREATE TABLE ftlc.event_prerequisites(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     event UUID REFERENCES ftlc.activities(id),
     prerequisite UUID REFERENCES ftlc.activities(id),
-    PRIMARY KEY(event,prerequisite)
+    CONSTRAINT unique_prerequisite UNIQUE (event,prerequisite)
 );
 
 COMMIT;
