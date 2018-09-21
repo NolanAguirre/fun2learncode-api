b7dfbab7b24aeda3d6a7fdc20c7c04db769ab8db
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-09-21 14:56:44 -0500

    add registration time frame to events

--- sqitch/deploy/events.sql
+++ sqitch/deploy/events.sql
@@ -7,6 +7,7 @@ CREATE TABLE ftlc.events(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     event_type UUID REFERENCES ftlc.activities(id),
     address UUID REFERENCES ftlc.address(id),
+    registration_time_frame UUID REFERENCES ftlc.date_interval(id),
     capacity INTEGER,
     price FLOAT
 );
