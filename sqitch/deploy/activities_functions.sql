ce116b95bf0caa912b0463ccf9ddec6a38c83bb6
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-09-27 15:07:46 -0500

    update null value to uuid null

--- sqitch/deploy/activities_functions.sql
+++ sqitch/deploy/activities_functions.sql
@@ -4,12 +4,12 @@
 
 BEGIN;
 
-CREATE FUNCTION ftlc.make_activity(UUID, CITEXT, CITEXT, UUID DEFAULT null) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION ftlc.make_activity(UUID, CITEXT, CITEXT, UUID DEFAULT '00000000-0000-0000-0000-000000000000') RETURNS VOID AS $$
     BEGIN
         INSERT INTO ftlc.activities (type, name, description) VALUES ($1, $2, $3);
-        IF($4 != null) THEN
+        IF($4 != '00000000-0000-0000-0000-000000000000') THEN
             INSERT INTO ftlc.event_prerequisites(event, prerequisite) VALUES($1, $4);
-        END IF; 
+        END IF;
     END;
 $$ LANGUAGE PLPGSQL STRICT;
 
