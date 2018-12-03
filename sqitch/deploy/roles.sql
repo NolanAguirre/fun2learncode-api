c6c270f124d3768a2b5c127436b73bfa8a262cd1
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-12-03 14:10:37 -0600

    reorder role grants so it works

--- sqitch/deploy/roles.sql
+++ sqitch/deploy/roles.sql
@@ -8,36 +8,36 @@ CREATE ROLE ftlc_admin_group;
 
 CREATE ROLE ftlc_owner;
 
-GRANT ftlc_owner to ftlc_admin_group;
+GRANT ftlc_admin_group to ftlc_owner;
 
 CREATE ROLE ftlc_admin;
 
-GRANT ftlc_admin to ftlc_admin_group;
+GRANT ftlc_admin_group to ftlc_admin;
 
 CREATE ROLE ftlc_lead_instructor;
 
-GRANT ftlc_lead_instructor to ftlc_admin_group;
+GRANT ftlc_admin_group to ftlc_lead_instructor;
 
-GRANT ftlc_admin_group to ftlc_roles;
+GRANT ftlc_roles to ftlc_admin_group;
 
 CREATE ROLE ftlc_attendant;
 
-GRANT ftlc_attendant to ftlc_roles;
+GRANT ftlc_roles to ftlc_attendant;
 
 CREATE ROLE ftlc_instructor;
 
-GRANT ftlc_instructor TO ftlc_roles;
+GRANT ftlc_roles TO ftlc_instructor;
 
 CREATE ROLE ftlc_user;
 
-GRANT ftlc_user TO ftlc_roles;
+GRANT ftlc_roles TO ftlc_user;
 
 CREATE ROLE ftlc_student;
 
-GRANT ftlc_student TO ftlc_roles;
+GRANT ftlc_roles TO ftlc_student;
 
 CREATE ROLE ftlc_anonymous;
 
-GRANT ftlc_anonymous TO ftlc_roles;
+GRANT ftlc_roles TO ftlc_anonymous;
 
 COMMIT;
