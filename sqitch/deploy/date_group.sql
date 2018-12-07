368ceddb10f517fd0bc045892ab29146f81bbcb9
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-12-07 16:37:23 -0600

    move price, capacity and address into date groups

--- sqitch/deploy/date_group.sql
+++ sqitch/deploy/date_group.sql
@@ -5,9 +5,12 @@ BEGIN;
 CREATE TABLE ftlc.date_group(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     event UUID REFERENCES ftlc.events(id),
-    name TEXT,
+    address UUID REFERENCES ftlc.address(id),
     open_registration TIMESTAMP,
-    close_registration TIMESTAMP
+    close_registration TIMESTAMP,
+    capacity INTEGER,
+    name TEXT,
+    price FLOAT
 );
 
 COMMIT;
--- sqitch/deploy/events.sql
+++ sqitch/deploy/events.sql
@@ -7,11 +7,8 @@ BEGIN;
 CREATE TABLE ftlc.events(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     event_type UUID REFERENCES ftlc.activities(id),
-    address UUID REFERENCES ftlc.address(id),
     open_registration TIMESTAMP,
-    close_registration TIMESTAMP,
-    capacity INTEGER,
-    price FLOAT
+    close_registration TIMESTAMP
 );
 
 COMMIT;
