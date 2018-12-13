bc6a267aa618f1f58fc36626679e6b418a601f61
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-12-13 13:15:03 -0600

    remove make activities function

--- sqitch/deploy/activities_functions.sql
+++ sqitch/deploy/activities_functions.sql
@@ -1,16 +1,6 @@
--- Deploy ftlc:activities_functions to pg
--- requires: activities
--- requires: event_prerequisites
+-- Deploy ftlc:activities to pg
+-- requires: activity_catagories
 
 BEGIN;
-
-CREATE OR REPLACE FUNCTION ftlc.make_activity(UUID, CITEXT, CITEXT, UUID DEFAULT '00000000-0000-0000-0000-000000000000') RETURNS VOID AS $$
-    BEGIN
-        INSERT INTO ftlc.activities (type, name, description) VALUES ($1, $2, $3);
-        IF($4 != '00000000-0000-0000-0000-000000000000') THEN
-            INSERT INTO ftlc.event_prerequisites(event, prerequisite) VALUES($1, $4);
-        END IF;
-    END;
-$$ LANGUAGE PLPGSQL STRICT;
 
 COMMIT;
