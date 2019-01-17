0d7a6b34122881ae4bd10d3605a4736f70431e65
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-01-16 19:48:37 -0600

    add url to activities

--- sqitch/deploy/activities.sql
+++ sqitch/deploy/activities.sql
@@ -6,6 +6,7 @@ BEGIN;
 CREATE TABLE ftlc.activities(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     type UUID REFERENCES ftlc.activity_catagories(id),
+    url CITEXT,
     name CITEXT,
     description CITEXT,
     public_display BOOLEAN DEFAULT true,
