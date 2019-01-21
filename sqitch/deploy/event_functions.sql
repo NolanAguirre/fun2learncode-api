81b264b544857628769e3f3e3704fcb30ff8d835
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-01-21 17:45:28 -0600

    add extra validation functions for event registration

--- sqitch/deploy/event_functions.sql
+++ sqitch/deploy/event_functions.sql
@@ -3,7 +3,7 @@
 -- requires: events
 
 BEGIN;
--- 
+--
 -- CREATE FUNCTION ftlc.get_check_in_options(lastName CITEXT) returns
 --
 -- $$ LANGUAGE PLPGSQL STABLE;
@@ -26,4 +26,19 @@ CREATE FUNCTION ftlc.check_prerequisites(UUID, UUID) RETURNS BOOLEAN AS $$
     END;
 $$ LANGUAGE PLPGSQL STABLE;
 
+CREATE FUNCTION ftlc.check_registration(UUID, UUID) RETURNS BOOLEAN AS $$
+    SELECT EXISTS(SELECT 1 FROM ftlc.event_registration WHERE date_group = $1 AND student = $2);
+$$ LANGUAGE SQL STABLE;
+
+CREATE FUNCTION ftlc.check_time (UUID, UUID) RETURNS BOOLEAN AS $$
+    DECLARE
+        dates UUID[];
+    BEGIN
+        SELECT array(
+            SELECT DISTINCT date_interval FROM ftlc.dates_join WHERE date_group IN (
+                SELECT date_group FROM ftlc.event_registration WHERE student = $2)) INTO dates;
+        SELECT EXISTS(SELECT 1 FROM ftlc.date_interval WHERE id IN (SELECT date_interval FROM ftlc.dates_join WHERE date_group = $1) AND id @> dates);
+    END;
+$$ LANGUAGE PLPGSQL STABLE;
+-- check time is not fool proof, it only finds events with the exact same timeslot, not that overlap, might be removed or improved later
 COMMIT;
--- sqitch/revert/event_functions.sql
+++ sqitch/revert/event_functions.sql
@@ -2,6 +2,10 @@
 
 BEGIN;
 
+DROP FUNCTION ftlc.check_registration(UUID, UUID);
+
+DROP FUNCTION ftlc.check_time (UUID, UUID);
+
 DROP FUNCTION ftlc.check_prerequisites(event_id UUID, student UUID);
 
 COMMIT;
