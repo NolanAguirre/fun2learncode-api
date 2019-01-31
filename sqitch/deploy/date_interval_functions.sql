4702b3c615c34c82fb57f589b2027721c5370770
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-01-30 21:58:45 -0600

    update date interval creation functions

--- sqitch/deploy/date_interval_functions.sql
+++ sqitch/deploy/date_interval_functions.sql
@@ -6,20 +6,23 @@ BEGIN;
 
 CREATE FUNCTION ftlc.make_date_interval(TIMESTAMP, TIMESTAMP, UUID) RETURNS ftlc.dates_join AS $$
     DECLARE
-       date_interval UUID;
+       dateInterval UUID;
        dates_join ftlc.dates_join;
    BEGIN
-   SELECT id INTO date_interval FROM ftlc.date_interval WHERE start = $1 AND "end" = $2;
-   IF(date_interval IS NULL) THEN
-       INSERT INTO ftlc.date_interval (start, "end") VALUES ($1, $2) RETURNING id INTO date_interval;
+   SELECT id INTO dateInterval FROM ftlc.date_interval WHERE start = $1 AND "end" = $2;
+   IF(dateInterval IS NULL) THEN
+       INSERT INTO ftlc.date_interval (start, "end") VALUES ($1, $2) RETURNING id INTO date_int;
    END IF;
-   INSERT INTO ftlc.dates_join (date_group, "date_interval") VALUES ($3, date_interval) RETURNING * INTO dates_join;
+   SELECT * INTO dates_join FROM ftlc.dates_join WHERE date_interval = dateInterval AND date_group = $3;
+   IF(dates_join IS NULL) THEN
+      INSERT INTO ftlc.dates_join (date_group, date_interval) VALUES ($3, date_int) RETURNING * INTO dates_join;
+   END IF;
    RETURN dates_join;
    END;
 $$ LANGUAGE PLPGSQL STRICT;
 
 CREATE FUNCTION ftlc.remove_date_interval(TIMESTAMP, TIMESTAMP, UUID) RETURNS ftlc.dates_join AS $$
-   DECLARE
+   DECLARE -- THIS MIGHT HAVE ISSUES, NEEDS MORE TESTING
       dateInterval UUID;
       dates_join ftlc.dates_join;
    BEGIN
