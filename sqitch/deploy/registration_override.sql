4a20d10c964c9a491103d276a1c3da827725f398
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-01-31 23:11:21 -0600

    update registration override to be for a date group

--- sqitch/deploy/registration_override.sql
+++ sqitch/deploy/registration_override.sql
@@ -7,7 +7,7 @@ BEGIN;
 CREATE TABLE ftlc.registration_override(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     student UUID REFERENCES ftlc.students(id),
-    activity UUID REFERENCES ftlc.activities(id),
+    date_group UUID REFERENCES ftlc.date_group(id),
     modified_price FLOAT
 );
 
