9afde814f3a0ec9986bbbab1ed2134a73392d997
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-02-22 19:22:03 -0600

    udpate event request

--- sqitch/deploy/event_request.sql
+++ sqitch/deploy/event_request.sql
@@ -7,12 +7,14 @@ BEGIN;
 CREATE TABLE ftlc.event_request(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID REFERENCES ftlc.users(id) NOT NULL,
+    event UUID REFERENCES ftlc.event(id),
     created_on TIMESTAMP DEFAULT NOW(),
     desired_location CITEXT NOT NULL,
+    pay_in_full BOOLEAN NOT NULL,
     desired_cost FLOAT NOT NULL,
     desired_capacity INTEGER NOT NULL,
     description CITEXT NOT NULL,
-    access_token TEXT NOT NULL,
+    access_token TEXT UNIQUE NOT NULL,
     status ftlc.request_type DEFAULT 'pending'
 );
 
