06a035c69aa0ac9b7e17a02254ed84baee970e4c
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-02-23 01:57:45 -0600

    force push because a merge conflict is broken

--- sqitch/deploy/event_request_functions.sql
+++ sqitch/deploy/event_request_functions.sql
@@ -4,7 +4,12 @@
 BEGIN;
 
 CREATE FUNCTION ftlc.event_by_token(TEXT) RETURNS ftlc.event AS $$
-    SELECT * FROM ftlc.event WHERE id = (SELECT event FROM ftlc.event_request WHERE access_token = $1 AND (status = 'accepted' OR  (status = 'pending' AND user_id = ftlc.get_id())));
+    SELECT * FROM ftlc.event WHERE id =
+        (SELECT event FROM ftlc.event_request WHERE access_token = $1 AND
+            ((status = 'accepted' AND user_id = ftlc.get_id())
+            OR
+            (SELECT EXISTS (SELECT * FROM ftlc.payment WHERE user_id = (SELECT user_id FROM ftlc.event_request WHERE access_token = $1) AND id IN (SELECT DISTINCT payment FROM ftlc.event_registration WHERE event = (SELECT event FROM ftlc.event_request WHERE access_token = $1) AND registered_by = (SELECT user_id FROM ftlc.event_request WHERE access_token = $1))))));
 $$ LANGUAGE SQL SECURITY DEFINER STABLE;
 
+--line 11 checks if the event has been paid for by the person who requested the event.
 COMMIT;
