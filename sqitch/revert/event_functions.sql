aca029984c1b0a40a35c8db0c28526a82b4d8c13
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-02-22 19:23:06 -0600

    add revert event in date

--- sqitch/revert/event_functions.sql
+++ sqitch/revert/event_functions.sql
@@ -6,6 +6,8 @@ DROP FUNCTION ftlc.check_registration(UUID, UUID);
 
 DROP FUNCTION ftlc.check_time (UUID, UUID);
 
-DROP FUNCTION ftlc.check_prerequisite(event_id UUID, student UUID);
+DROP FUNCTION ftlc.check_prerequisite(UUID, UUID);
+
+DROP FUNCTION ftlc.event_in_dates(TIMESTAMP, TIMESTAMP);
 
 COMMIT;
