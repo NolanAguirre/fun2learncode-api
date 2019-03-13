d7d91a5f5049325e6b63f554869d7c6c15b55a93
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-03-12 19:04:22 -0500

    use jest to create and teardown test database

--- test/index.test.js
+++ test/index.test.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 const authenticate = require('./authenticate/authenticate.test')
 const begin = require('./begin/begin.test')
 const mailing = require('./mailing/mailing.test')
-const process = require('./process/process.test')
+const processTransaction = require('./process/process.test')
 const recover = require('./recover/recover.test')
 const refund = require('./refund/refund.test')
 const integration = require('./integration.test')
+const { execSync } = require('child_process')
+const database = require('../db')
 const units = [
-    process,
+    processTransaction,
     begin,
     refund,
     authenticate,
@@ -16,6 +18,19 @@ const units = [
     integration
 ]
 describe('Entire website test', () => {
+    beforeAll(() => {
+        const db =`${process.env.DATABASE_URL}`
+        execSync(`pg_dump -n ftlc -n ftlc_private --clean -d ${db} -f temp.sql`)
+        execSync(`psql -d ${db} -f testData.sql`)
+        return
+    });
+    afterAll(() => {
+        const db =`${process.env.DATABASE_URL}`
+        execSync(`psql -d ${db} -f temp.sql`)
+        execSync(`rm temp.sql`)
+        database.database.$pool.end()
+        return
+    })
     units.forEach((item)=>{
         item.process(describe, test, expect, item)
     })
