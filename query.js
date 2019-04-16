20ebe1f9dda231e45b487284437d080814148110
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-04-16 16:03:39 -0500

    update promo code query to check for prior uses

--- query.js
+++ query.js
@@ -1,5 +1,5 @@
 const promoCode = `SELECT effect, percent, code, id FROM ftlc.promo_code WHERE disabled = false AND valid_start < $1 AND $1 < valid_end AND uses > 0  AND code = $2 AND
-NOT(SELECT EXISTS(SELECT 1 FROM ftlc.promo_code_use WHERE promo_code = id AND user_id = $4)) AND
+(id NOT IN (SELECT promo_code FROM ftlc.promo_code_use WHERE user_id = $4)) AND
 ((for_category = false OR(for_category = true AND category = (SELECT category FROM ftlc.activity WHERE id = (SELECT activity FROM ftlc.event WHERE id = $3)))) AND
 (for_user = false OR (for_user = true AND user_id = $4)) AND
 (for_activity = false OR (for_activity = true AND activity = (SELECT activity FROM ftlc.event WHERE id = $3))))`
